090186e353736c017e5e97b1bc25cc9b5a637583
Ikechukwu Achom <ikechukwu.achom@kodehauz.com>
2021-09-21 12:01:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dictionaryappdemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.dictionaryappdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dictionaryappdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+
+    }
+}
